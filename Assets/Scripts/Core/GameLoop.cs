b8c56d3c888a996b6e0d92f87c4a23c9b2922ba5
Sergey Reshetnikov <s.reshetnikov@redbrixwall.com>
2026-03-17 00:08:15 +0300

    Update GameLoop and IGameSystem interface

--- Assets/Scripts/Core/GameLoop.cs
+++ Assets/Scripts/Core/GameLoop.cs
@@ -1,17 +1,31 @@
 using IncrementalRPG.Scripts.Reflex;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace IncrementalRPG.Scripts.Core
 {
     public class GameLoop : IStartable, ITickable
     {
+        private readonly List<IGameSystem> _systems;
+
+        public GameLoop(IEnumerable<IGameSystem> systems)
+        {
+            _systems = systems.ToList();
+        }
+
         public void OnStart()
         {
-            Debug.Log("[GameLoop] Started");
+            foreach (var t in _systems)
+                t.Initialize();
+
+            Debug.Log($"[GameLoop] Started. Systems: {_systems.Count}");
         }
 
         public void Tick(float deltaTime)
         {
+            foreach (var t in _systems)
+                t.Update(deltaTime);
         }
     }
 }
---
+++ Assets/Scripts/Core/IGameSystem.cs
@@ -0,0 +1,8 @@
+namespace IncrementalRPG.Scripts.Core
+{
+    public interface IGameSystem
+    {
+        void Initialize();
+        void Update(float deltaTime);
+    }
+}
